845fd32d80c66f4212e7a873eecc4e4b3f47deee
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-08-19 11:27:56 +0530

    Update 121. Best Time to Buy and Sell Stock.py

--- Blind75/Sliding Window/121. Best Time to Buy and Sell Stock.py
+++ Blind75/Sliding Window/121. Best Time to Buy and Sell Stock.py
@@ -10,4 +10,16 @@ class Solution:
                 l=r
             r+=1
         return maxp
+
+#the above is a two pointer/ sliding window solution. A DP solution to this would be directly using the value in the array and not pointing the values using indexes with same logic
+# DP solution
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minbuy=prices[0]
+        maxp=0
+        for sell in prices:
+            maxp=max(maxp,sell-minbuy)
+            minbuy=min(minbuy,sell)
+        return maxp
             
