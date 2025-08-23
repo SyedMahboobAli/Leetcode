f6e9e698ccdecb76ea0136b5ddf3a9208d603a38
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-08-23 11:23:43 +0530

    Update 53. Maximum Subarray.py

--- Blind75/Arrays/53. Maximum Subarray.py
+++ Blind75/Arrays/53. Maximum Subarray.py
@@ -9,3 +9,25 @@ class Solution:
                 currsum=0
             
         return maxsum
+## follow up print array as well
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        highest_total=nums[0]
+        highest_seq=[]
+        curr_sum=0
+        curr_seq=[]
+        for n in nums:
+            curr_sum+=n
+            curr_seq.append(n)
+            temp=highest_total
+            highest_total=max(highest_total,curr_sum)
+            if(highest_total!=temp):
+                highest_seq=curr_seq[:]
+            if curr_sum<0:
+                curr_sum=0
+                curr_seq=[]
+        print(highest_seq)
+        return highest_total
+
+            
+        return maxsum
