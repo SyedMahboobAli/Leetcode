bd0e2d14b27f19cd334fd50e731d766659dc2c17
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-02 00:16:03 +0530

    Create 347. Top K Frequent Elements.py

---
+++ NeetCode 150/Arrays/347. Top K Frequent Elements.py
@@ -0,0 +1,27 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d={}
+        # Step 1: Count frequency
+        for i in nums:
+            if i in d:# or d[i]=1+d.get(i,0)
+                d[i]+=1
+            else:
+                d[i]=1
+        # use d=Counter(nums)
+        frequency=[[] for _ in range(len(nums)+1)]
+#We're using bucket sort where the index of the outer list represents the frequency of elements.
+#The maximum frequency any number can have is if all numbers are the same.
+#So, in the worst case, one number appears len(nums) times.
+
+        # Step 2: Create frequency table where index = frequency
+        for num,freq in d.items():
+            frequency[freq].append(num)
+        
+        # Step 3: Collect top k frequent elements
+        res=[]
+        for i in range(len(frequency)-1,0,-1):
+            for num in frequency[i]:
+                res.append(num)
+                if(len(res)==k):
+                    return res
+        
