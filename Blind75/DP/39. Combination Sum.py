531fd6ce5761d0aa9d433e31033e347b4b968d21
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-16 19:10:50 +0530

    Create 39. Combination Sum.py

---
+++ Blind75/DP/39. Combination Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp=[[]]* (target+1)
+        dp[0]=[[]]
+        candidates.sort()
+        for c in candidates:
+            for t in range(c,target+1):
+                for comb in dp[t-c]:
+                    new_comb=comb+[c]
+                     # Add c to previous combination
+                    dp[t].append(new_comb)
+        return dp[target]
+'''
+This is throwing error. Memory Limit Exceeded
+
+Count combinations	DP
+Print all combinations	✅ Backtracking (LeetCode #39)
+Each number used once (no repeats)	Backtracking + skip duplicates (LeetCode #40)
+'''
+
+# Backtracking Solution
