9631cb39ea4e14fab22a8724a03425440d5195b7
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-17 22:22:31 +0530

    Update 62. Unique Paths.py

--- Blind75/DP/62. Unique Paths.py
+++ Blind75/DP/62. Unique Paths.py
@@ -1,5 +1,23 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # better solution for space using 1D array
+
+        dp=[1]*(n)
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j]=dp[j]+dp[j-1]
+        return dp[-1]
+
+        '''
+        At any cell (i, j):
+
+dp[j] holds paths from above (old value before update)
+
+dp[j-1] holds paths from left (just updated)
+So we correctly combine both directions, overwriting as we go left→right.
+and first is set to 1 anyway, but the first column is always 1 as we second loop we are always starting from index 1
+        '''
+        #2D dp solution
         dp=[[0] * (n) for _ in range(m)]
         for i in range(m):
             for j in range(n):
