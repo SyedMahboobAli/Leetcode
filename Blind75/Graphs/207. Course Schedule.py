e61c11505e7305a0a810f440c59080fd3bdcfee2
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-22 01:16:22 +0530

    Create 207. Course Schedule.py

---
+++ Blind75/Graphs/207. Course Schedule.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Build adjacency list representation of the course graph
+        adj={i:[] for i in range(numCourses)}
+        # To take 'crs', you must first take 'pre'
+        for crs,pre in prerequisites:
+            adj[crs].append(pre)
+        
+        # visited states: 
+        # 0 = unvisited, 1 = visiting (in current recursion stack), 2 = visited (fully processed)
+        visited = [0 for _ in range(numCourses)] # or [0] * numCourses
+
+        def dfs(course):
+            # If course is being visited again in current DFS path → cycle detected
+            if(visited[course] == 1):
+                return False
+            # If already processed completely, skip
+            if(visited[course] == 2):
+                return True
+            
+            # Mark current node as visiting
+            visited[course]=1
+
+            # Recursively visit all pre courses
+            for nei in adj[course]:
+                if not dfs(nei):
+                     # Cycle found deeper in recursion
+                    return False
+                    
+            # After exploring all neighbors, mark as fully processed
+            visited[course]=2
+            return True
+
+        # Run DFS for all courses (graph may be disconnected)
+        for i in range(numCourses):
+            if not dfs(i):
+                return False 
+                  
+        return True
