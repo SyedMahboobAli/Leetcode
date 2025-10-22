6fd4d04eadd9f402a17c043637dbe388c47607a5
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-22 21:12:22 +0530

    Update 200. Number of Islands.py

--- Blind75/Graphs/200. Number of Islands.py
+++ Blind75/Graphs/200. Number of Islands.py
@@ -1,4 +1,4 @@
-#DFS Solution, here the approach is using 4 dfs calls in all directions. Other approach is similar to pacific atlantic solution
+#DFS Solution, here the approach is using 4 dfs calls in all directions. Other approach is similar to pacific atlantic solution also present in BFS approach
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
@@ -28,3 +28,33 @@ class Solution:
         return count
 
 #BFS Solution
+
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        count=0
+        n,m=len(grid),len(grid[0])
+        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+
+        def bfs(r,c):
+            q=deque()
+            q.append((r,c))
+            grid[r][c]='0' # marking as visited
+            while q:
+                x,y=q.popleft()
+                for dr,dc in directions:
+                    nr,nc=dr+x,dc+y
+                    if(0<=nr<n and 0<=nc<m and grid[nr][nc]=='1'):
+                        grid[nr][nc]='0'
+                        q.append((nr,nc))
+ 
+        for i in range(n):
+            for j in range(m):
+                if(grid[i][j]=='1'): # found an unvisited land
+                    count+=1
+                    bfs(i,j)
+                
+        return count
