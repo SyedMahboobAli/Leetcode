cdb2e37d2e30b672e0825cc0540b52c7671bcc1b
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-10-22 21:01:21 +0530

    Create 200. Number of Islands.py

---
+++ Blind75/Graphs/200. Number of Islands.py
@@ -0,0 +1,30 @@
+#DFS Solution, here the approach is using 4 dfs calls in all directions. Other approach is similar to pacific atlantic solution
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+          return 0
+        count=0
+
+        n,m=len(grid),len(grid[0])
+
+        def dfs(r,c):
+            # Check boundaries and water/visited cells
+            if(r<0 or c<0 or r>=n or c>=m or grid[r][c] != '1'):
+                return
+            #So we are only visiting the cells that are 1
+            grid[r][c]='0' # mark as visited by making it 0
+            # Explore all 4 directions
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        
+        for i in range(n):
+            for j in range(m):
+                if(grid[i][j]=='1'): # found an unvisited land
+                    count+=1
+                    dfs(i,j)
+                
+        return count
+
+#BFS Solution
