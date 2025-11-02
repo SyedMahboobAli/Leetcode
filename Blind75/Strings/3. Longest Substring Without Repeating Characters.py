3b9570576e4ec92d30ceed0db65fc315ede7a779
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-11-02 12:20:39 +0530

    Create 3. Longest Substring Without Repeating Characters.py

---
+++ Blind75/Strings/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left=0 # Left pointer
+        max_length=0 #float('-inf') not needed as "" is zero
+        char_set = set() # To store current window characters
+
+        for right in range(len(s)):  # Right pointer moves forward
+            if(s[right] in char_set): # If duplicate found
+                while s[right] in char_set:
+                    char_set.remove(s[left])
+                    left+=1
+            char_set.add(s[right]) # Add current character
+            max_length=max(max_length,right-left+1) # or max_length=max(max_length,len(char_set))
+        
+        return max_length
+
+        #approach 2: use dictionary
+
+        mp = {} # Stores last seen index of each character
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l) # Move left pointer past the duplicate
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
