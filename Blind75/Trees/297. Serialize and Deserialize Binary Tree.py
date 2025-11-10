b1b3fc8bf55b8ec6bf275ece708cdb9ea7a2ab6f
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-11-11 00:06:46 +0530

    Create 297. Serialize and Deserialize Binary Tree.py

---
+++ Blind75/Trees/297. Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        vals=[]
+
+        def dfs(node):
+            if not node:
+                vals.append("null")
+                return
+            vals.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return ",".join(vals)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+
+        vals = iter(data.split(",")) #iterator over tokens
+        def dfs():
+            val = next(vals)
+            if val == "null":
+                return None
+            node = TreeNode(int(val))
+            node.left=dfs()
+            node.right=dfs()
+            return node
+            
+        root = dfs()
+        return root
+        '''
+        or 
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
+        '''
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
