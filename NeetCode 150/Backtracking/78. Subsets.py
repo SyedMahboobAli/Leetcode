e54050ff631a6c198e24a219cf94497b6ca235a8
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2025-12-27 11:54:22 +0530

    Update 78. Subsets.py

--- NeetCode 150/Backtracking/78. Subsets.py
+++ NeetCode 150/Backtracking/78. Subsets.py
@@ -1,5 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        #Solution model 2: loop. Used in other problems as well
+        res = []
+        subset = []
+
+        def backtrack(start):
+            # Every path is a valid subset
+            res.append(subset[:])
+
+            for i in range(start,len(nums)):
+                subset.append(nums[i])
+                backtrack(i+1)
+                subset.pop()
+        backtrack(0)
+        return res
+        #Solution 1
         res = []
         subset = []
 
