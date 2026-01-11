631b5b27893151212424e9287cf2d5edb64d883e
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2026-01-11 12:26:15 +0530

    Create 322. Coin Change.py

---
+++ NeetCode 150/DP/322. Coin Change.py
@@ -0,0 +1,8 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin,amount+1):
+                dp[i] = min(dp[i],1+dp[i-coin]) #i is the amount we are trying to make
+        return dp[amount] if dp[amount] != float('inf') else -1
