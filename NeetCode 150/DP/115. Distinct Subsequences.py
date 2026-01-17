57d961049d21026d41ca952f79a3d4c9a70ebdff
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2026-01-17 16:39:30 +0530

    Create 115. Distinct Subsequences.py

---
+++ NeetCode 150/DP/115. Distinct Subsequences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        #1D solution
+        m,n = len(s),len(t)
+        dp = [0] * (n+1)
+        dp[0] = 1 #empty t
+
+        for i in range(1,m+1):
+            for j in range(n,0,-1): # we go reverse because we use i-1 and j-1 which are prev values and already computed we might get wrong ans
+                if s[i-1] ==t[j-1]:
+                    dp[j] += dp[j-1]
+        return dp[n]
+        #2D DP sol
+        m,n = len(s),len(t)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        #Base Case
+        for i in range(m+1):
+            dp[i][0]=1
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[m][n]
