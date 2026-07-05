b61d47b8105ca9a5da8dccbb7658e2cf0e63f03f
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2026-07-05 18:54:14 +0530

    Update 2380. Time Needed to Rearrange a Binary String.py

--- Company Wise Problems/Salesforce/2380. Time Needed to Rearrange a Binary String.py
+++ Company Wise Problems/Salesforce/2380. Time Needed to Rearrange a Binary String.py
@@ -1,3 +1,21 @@
+'''Instead of simulating every second, count how long each '1' needs to move past the '0's before it.
+
+Observation
+Every '1' must cross all preceding '0's.
+Let:
+zeros = number of '0's seen so far.
+time = minimum seconds required so far.
+
+When we encounter a '1':
+
+If there are no preceding zeros, it doesn't need to move.
+Otherwise:
+It needs at least zeros swaps.
+But if the previous '1' is still moving, this '1' must wait.
+Hence:
+time = max(time + 1, zeros)
+imagine for 0111 to understand the logic
+'''
 class Solution:
     def secondsToRemoveOccurrences(self, s: str) -> int:
         zeroes = 0
