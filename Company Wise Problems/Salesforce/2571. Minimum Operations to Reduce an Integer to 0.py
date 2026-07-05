76c85f675deb38bdd0fe8726327ab4dbddc25100
SyedMahboobAli <72000407+SyedMahboobAli@users.noreply.github.com>
2026-07-05 19:12:21 +0530

    Update 2571. Minimum Operations to Reduce an Integer to 0.py

--- Company Wise Problems/Salesforce/2571. Minimum Operations to Reduce an Integer to 0.py
+++ Company Wise Problems/Salesforce/2571. Minimum Operations to Reduce an Integer to 0.py
@@ -1,7 +1,7 @@
 '''
 At every step:
 If n is even → divide by 2
-If n is odd:
+If n is odd: then last 2 digits of odd can be 11 or 01
 If last two bits are 11 → add 1 and increase res
 Otherwise last two bits are 01 → subtract 1 and increase res
 That’s it.
@@ -11,7 +11,7 @@ class Solution:
         res = 0
         while n > 0:
             if n % 2 == 0:
-                n >>= 1
+                n >>= 1 #this is divide by 2. Since we can do this any number of times (power of 2 times) we are not incrementing res and also when we add or subtract from 'n' only then the operation is counted. now in the next scenarios for odd, we are only incrementing 1 or decrementing 1. imagine that here we are subtracting 2 power x and then also subtracting 2 power 0. cumulative that operation is 1. 
             elif (n & 3) == 3:
                 n += 1
                 res += 1
